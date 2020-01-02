1cde34aa7aa2c2358d9c5cc38d3624c75c16d332
mraffiramdhani <raffi.programming@gmail.com>
2020-01-02 23:57:10 +0700

    Adding CustomTable Component

--- src/pages/users/Index.js
+++ src/pages/users/Index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CustomTable from '../../components/Content/CustomTable';
 
 const UserIndex = (props) => {
 
     return (
         <div>
             Ini User Index
+            <CustomTable />
         </div>
     )
 
