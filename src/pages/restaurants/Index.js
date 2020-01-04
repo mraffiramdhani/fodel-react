2cad71d3f5467a524f43924451c6357dbb71b871
mraffiramdhani <raffi.programming@gmail.com>
2020-01-04 18:02:30 +0700

    Implementing React-Table

--- src/pages/restaurants/Index.js
+++ src/pages/restaurants/Index.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import RestaurantTable from '../../components/Content/RestaurantTable';
+import React, { useState, useMemo } from 'react';
+// import RestaurantTable from '../../components/Content/RestaurantTable';
 import { Link } from 'react-router-dom';
 import Modal from '../../components/Modal/Modal';
+import Table from '../../components/Content/Table';
+import DummyRestaurant from '../../data/restaurant';
 
 const RestaurantIndex = () => {
 
@@ -11,13 +13,55 @@ const RestaurantIndex = () => {
         setModalOpen(!isModalOpen)
     }
 
+    const columns = useMemo(() => [
+        {
+            Header: 'ID',
+            accessor: 'id'
+        },
+        {
+            Header: 'Name',
+            accessor: 'name'
+        },
+        {
+            Header: 'Logo',
+            accessor: 'logo',
+            Cell: ({ row }) => (
+                <div>
+                    <img alt={row.original.name} src={row.original.logo} width="40" height="40" />
+                </div>
+            )
+        },
+        {
+            Header: 'Location',
+            accessor: 'longitude',
+            Cell: ({ row }) => (
+                <span>Long: {row.original.longitude} | Lat: {row.original.latitude}</span>
+            )
+        },
+        {
+            Header: 'Description',
+            accessor: 'description'
+        },
+        {
+            Header: 'Option',
+            id: 'option',
+            accessor: 'id',
+            Cell: ({ row }) => (
+                <div>
+                    <Link to="/admin/restaurant/edit/" className="btn btn-warning"><i className="fa fa-edit"></i></Link>{" "}
+                    <Link to="#" className="btn btn-danger" onClick={handleModalOpen} > <i className="fa fa-trash"></i> </Link >
+                </div>
+            ),
+        }
+    ], [])
+
     return (
         <div>
             <Modal isOpen={isModalOpen} isToggled={handleModalOpen} title="Delete Restaurant" isType="delete">
                 This action cannot be undone. Continue?
             </Modal>
             <Link to="/admin/restaurant/create" className="btn btn-success btn-block mt-3"><i className="fa fa-plus"></i> Add New</Link>
-            <RestaurantTable openModal={handleModalOpen} />
+            <Table columns={columns} data={DummyRestaurant.requests} />
         </div>
     )
 
--- src/pages/users/Index.js
+++ src/pages/users/Index.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
-import UserTable from '../../components/Content/UserTable';
+import React, { useState, useMemo, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import Modal from '../../components/Modal/Modal';
+import Table from '../../components/Content/Table';
+import DummyUser from '../../data/user';
 
 const UserIndex = () => {
 
     const [isModalOpen, setModalOpen] = useState(false)
 
-    const handleModalOpen = () => {
+    const handleModalOpen = useCallback(() => {
         setModalOpen(!isModalOpen)
-    }
+    }, [isModalOpen])
+
+    const columns = useMemo(() => [
+        {
+            Header: 'ID',
+            accessor: 'id'
+        },
+        {
+            Header: 'Name',
+            accessor: 'name'
+        },
+        {
+            Header: 'Username',
+            accessor: 'username'
+        },
+        {
+            Header: 'Option',
+            id: 'option',
+            accessor: 'id',
+            Cell: ({ row }) => (
+                <div>
+                    <Link to="/admin/user/edit/" className="btn btn-warning"><i className="fa fa-edit"></i></Link>{" "}
+                    <Link to="#" onClick={handleModalOpen} className="btn btn-danger" > <i className="fa fa-trash"></i> </Link >
+                </div>
+            ),
+        }
+    ], [handleModalOpen])
 
     return (
         <div>
@@ -17,7 +44,7 @@ const UserIndex = () => {
                 This action cannot be undone. Continue?
             </Modal>
             <Link to="/admin/user/create" className="btn btn-success btn-block mt-3"><i className="fa fa-plus"></i> Add New</Link>
-            <UserTable openModal={handleModalOpen} />
+            <Table columns={columns} data={DummyUser} sortable fillterable />
         </div>
     )
 
