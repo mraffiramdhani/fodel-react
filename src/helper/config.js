6ea0e3801e9bd2e984619597011a53a941c7f9a0
mraffiramdhani <raffi.programming@gmail.com>
2020-02-07 10:40:22 +0700

    config build

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export const APP_URL = "http://localhost:4040/api/v1"
-export const APP_ICON_URL = "http://localhost:4040/icons"
-export const APP_IMAGE_URL = "http://localhost:4040/images"
+export const APP_URL = "http://52.22.124.137:4300/api/v1"
+export const APP_ICON_URL = "http://52.22.124.137:4300/icons"
+export const APP_IMAGE_URL = "http://52.22.124.137:4300/images"
 
 export const Get = (url, contentType = 'application/json') => {
     return new Promise((resolve, reject) => {
